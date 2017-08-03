189a6c593c1c77a3379ef08a099d1bd01463d022
ShikangGan <287752151@qq.com>
2017-08-03 11:17:47 +1000

    Update Distance.py

--- Distance.py
+++ Distance.py
@@ -3,39 +3,72 @@ import numpy as np
 import skimage
 from skimage import draw
 import DPP 
+import LineIntersection
 import copy 
 Width=600
 Height=400
+import time
 
-def GridentDescent(polygons):
-    learning_rate=2
-    center_vectors=DPP.DPP(20,len(polygons))
-    for i in range(0,len(center_vectors)):
-        if i%2==1:
-            center_vectors[i]*=Width
-        else:
-            center_vectors[i]*=Height
+total_time = [1]
+
+def GridentDescent(center_vectors,polygons,learning_rate,value):
+
     flag=True
     while flag:
-        t=2.0
+        t=0.5
         flag=False
-        c1=copy.copy(center_vectors)
+
         current_vectors=copy.copy(center_vectors)
+        
+        
         for i in range(0,len(center_vectors)):
+            
+            c1=copy.copy(center_vectors)
             c1[i]+=t
-            # a=ObjectiveFunction(tuple(c1),polygons)
-            # b=ObjectiveFunction(tuple(center_vectors),polygons)
-            # det=(a-b)/t
+
+
             det=(ObjectiveFunction(tuple(c1),polygons)-ObjectiveFunction(tuple(center_vectors),polygons))/t 
-            center_vectors[i]+=(learning_rate*det)
-        print current_vectors
-        for i in range(0,len(current_vectors)):
-            if abs(current_vectors[i]-center_vectors[i])>=1:
-                flag=True
-                break
-    return current_vectors
+
+
+
+            # center_vectors[i]+=(learning_rate[i]*det)
+            # center_vectors[i]=int(center_vectors[i]-(learning_rate*det))
+
+            if det>0:
+                # learning_rate[i]*=1.5
+                center_vectors[i]+=(learning_rate[i]*det)                
+            else:
+                # learning_rate[i]*=0.8
+                center_vectors[i]+=(learning_rate[i]*det) 
+                # if learning_rate[i]<0.001:
+                #     learning_rate[i]=1   
+
+            if isPolygonOutOfBound(center_vectors[i/2*2],center_vectors[i/2*2+1],polygons[i/2]):
+                center_vectors[i]=current_vectors[i]
+                # learning_rate[i]=0.01
+
+    
+        return ObjectiveFunction(tuple(center_vectors),polygons)
+
+
+
+
+
+def isPolygonOutOfBound(x,y,polygon):
+    for i in range(0,len(polygon)):
+        if i%2==0:
+            a=polygon[i]+x
+            if a<0 or a>Width:
+                return True
+        else:
+            a=polygon[i]+y
+            if a<0 or a>Height:
+                return True
+    return False
+
 
 def ObjectiveFunction(center_vectors,polygons):
+    
     new_polygons=[]
     for i in range(0,len(center_vectors)/2):
         temp=[]
@@ -45,7 +78,12 @@ def ObjectiveFunction(center_vectors,polygons):
             else:
                 temp.append(polygons[i][j]+center_vectors[2*i+1])
         new_polygons.append(tuple(temp))
-    return SumOfDistances(new_polygons)
+    
+    # return SumOfDistances(new_polygons)+0.5*MinOfDistances(new_polygons)
+
+   
+    result = test(new_polygons)
+    return result
 
 
 
@@ -54,13 +92,37 @@ def SumOfDistances (polygons):
     su=float(0)
     for i in range(0,len(polygons)):
         for j in range(i+1,len(polygons)):
-            su+=Distance_polygons(polygons[i],polygons[j])
+            su+=Distance_polygons(polygons[i],polygons[j],True)
     return su
+def MinOfDistances(polygons):
+    temp=[]
+    for i in range(0,len(polygons)):
+        for j in range(i+1,len(polygons)):
+            temp.append(Distance_polygons(polygons[i],polygons[j],False))
+    return min(temp)
+def ProdOfDistance(polygons):
+    po=float(1)
+    for i in range(0,len(polygons)):
+        for j in range(i+1,len(polygons)):
+            po*=Distance_polygons(polygons[i],polygons[j],True)
+    return po
+
+def test(polygons):
+    mi=0.0
+    su=0.0
+    for i in range(0,len(polygons)):
+        temp=[]
+        for j in range(0,len(polygons)):
+            if i!=j:
+                temp.append(Distance_polygons(polygons[i],polygons[j],True))
+        mi+=min(temp)
+        su+=sum(temp)
+    return mi+su/len(polygons)/2
 
 
-def Distance_polygons (A,B):
-    # A=points[0]
-    # B=points[1]
+def Distance_polygons (A,B,flag):
+    #flag False means do not consider the overlap problem
+
     Ax=A[0::2]
     Ay=A[1::2]
     Bx=B[0::2]
@@ -69,12 +131,19 @@ def Distance_polygons (A,B):
     Bedge=[]
     dist=[]
     
+
+    l1 = time.time()
+   
+
+
     for i in range(0,len(Ax)-1):
         Aedge.append(tuple((Ax[i],Ay[i],Ax[i+1],Ay[i+1])))
     Aedge.append(tuple((Ax[-1],Ay[-1],Ax[0],Ay[0])))
     for i in range(0,len(Bx)-1):
         Bedge.append(tuple((Bx[i],By[i],Bx[i+1],By[i+1])))
     Bedge.append(tuple((Bx[-1],By[-1],Bx[0],By[0])))
+    l2 = time.time()
+    
     # print Aedge,Bedge
     for i in range(0,len(Ax)):
         for j in range(0,len(Bedge)):
@@ -82,36 +151,41 @@ def Distance_polygons (A,B):
     for i in range(0,len(Bx)):
         for j in range(0,len(Aedge)):
             dist.append(Distance_Point2LineSeg(Aedge[j],Bx[i],By[i]))
-
-    if isPolygonsOverlap(A,B):
-        return (-1)*min(dist)
+    
+    if flag:
+        if isPolygonsOverlap(A,B):
+            return (-1)*(min(dist))*20
+        else:
+            return min(dist)
     else:
         return min(dist)
 
+
+
+
 def isPolygonsOverlap(A,B):
 
-    Ax=np.asarray(A[0::2])
-    Ay=np.asarray(A[1::2])
-    Bx=np.asarray(B[0::2])
-    By=np.asarray(B[1::2])
+    # return True means overlap
 
-    x1,y1=skimage.draw.polygon(Ax,Ay)
-    x2,y2=skimage.draw.polygon(Bx,By)
-
-    x=max(max(x1),max(x2))+1
-    y=max(max(y1),max(y2))+1
+    Ax=A[0::2]
+    Ay=A[1::2]
+    Bx=B[0::2]
+    By=B[1::2]
+    Aedge=[]
+    Bedge=[]
     
-    test=np.zeros((x,y),dtype=int)
+    for i in range(0,len(Ax)-1):
+        Aedge.append(tuple((Ax[i],Ay[i],Ax[i+1],Ay[i+1])))
+    Aedge.append(tuple((Ax[-1],Ay[-1],Ax[0],Ay[0])))
+    for i in range(0,len(Bx)-1):
+        Bedge.append(tuple((Bx[i],By[i],Bx[i+1],By[i+1])))
+    Bedge.append(tuple((Bx[-1],By[-1],Bx[0],By[0])))
 
-    test[x1,y1] +=1
-    test[x2,y2] +=1
-
-    for i in range(0,x):
-        for j in range(0,y):
-            if test[i][j]==2:
+    for i in range(0,len(Aedge)):
+        for j in range(0,len(Bedge)):
+            if LineIntersection.IfLineIntersect(Aedge[i],Bedge[j]):
                 return True
     return False
-    
 
 
 
@@ -132,12 +206,7 @@ def Distance_Point2LineSeg (edge, x3,y3): # x3,y3 is the point
     y = y1 + u * py
     dx = x - x3
     dy = y - y3
-    # Note: If the actual distance does not matter,
-    # if you only want to compare what this function
-    # returns to other results of this function, you
-    # can just return the squared distance instead
-    # (i.e. remove the sqrt) to gain a little performance
+
     dist = math.sqrt(dx*dx + dy*dy)
-    return dist   
-GridentDescent(((0,0,130,0,0,150),(60,0,0,120,-60,0,-30,0,-30,-60,30,-60,30,0),(60,0,0,50,-60,0,-30,-40,30,-40)))
-print 'finish'
+    return dist
+
